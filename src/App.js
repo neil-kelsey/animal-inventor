0c70f6d97d9a28539bf8c3d54b8fc033cd3b22c6
Neil Kelsey <neil.kelsey@gmail.com>
2024-04-21 13:08:09 +0100

    feat: Adding boiler plate project files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const animals = ['lion', 'tiger', 'bear', 'monkey', 'panda', 'zebra', 'giraffe'];
+
+  // Define a function to generate a random animal name
+
+  // Return the JSX element to be rendered
+  return (
+    <div className="App">
+      <p>animal name here</p>
+      <button>new animal</button>
+    </div>
+  );
+}
+
+export default App;
